434be83ca079af46b29e810f756ba54750e0c0f4
AbedAnaam <khurin.anaam@gmail.com>
2024-05-12 07:24:35 +0700

    L2-C1

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const product = 'Socks'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Books',
+            description : 'This book are so good'
+        }
+    }
+})
